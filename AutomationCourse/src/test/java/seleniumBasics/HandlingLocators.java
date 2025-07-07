4b817b6da871ecb47b0d73779719bc95a23232d1
Netcom <Netcom@DESKTOP-16JJG09>
2025-07-07 15:44:51 +0530

    Name,Linktext,PartialLinkText,Xpath,CSSSelector

--- AutomationCourse/src/test/java/seleniumBasics/HandlingLocators.java
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingLocators.java
@@ -5,10 +5,21 @@ import org.openqa.selenium.By;
 public class HandlingLocators extends Base {
 
 	public void verifyLocators() {
-		//Locate an element with id, class name, tagname
+		//Locate an element with id, class name, tagname etc
 		driver.findElement(By.id("button-one"));
 		driver.findElement(By.className("btn btn-primary"));
 		driver.findElement(By.tagName("button"));
+		driver.findElement(By.name("viewport"));
+		driver.findElement(By.linkText("Simple Form Demo"));
+		driver.findElement(By.partialLinkText("Simple"));
+		driver.findElement(By.cssSelector("button[id='button-one']"));
+		driver.findElement(By.xpath("//button[@id='button-one']"));
+		driver.findElement(By.xpath("//button[text()=='Get Total']"));
+		driver.findElement(By.xpath("//button[starts-with(text(),'Show ')]"));
+		
+		//Combine more than 1 attribute to get 1 of 1 for xpath written
+		driver.findElement(By.xpath("//button[@id='button-one' and @type='button']"));
+		driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));
 	}
 	
 	public static void main(String[] args) {
