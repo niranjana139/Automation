2f3dd13b95b8167f3284aa4038ea6b168d205399
Netcom <Netcom@DESKTOP-16JJG09>
2025-07-15 12:03:15 +0530

    TestNG Annotations,Executor

---
+++ AutomationCourse/src/test/java/seleniumBasics/TestNgAnnotations.java
@@ -0,0 +1,63 @@
+package seleniumBasics;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNgAnnotations {
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before class");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before suite");
+	}
+	
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After suite");
+	}
+	
+	
+	@Test
+	public void testCase() {
+		System.out.println("TestCase");
+	}
+	
+
+}
