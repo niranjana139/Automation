aaca3134b1b3d4b1d1088a85b84271ce7b0c722b
Netcom <Netcom@DESKTOP-16JJG09>
2025-07-17 11:58:56 +0530

    Excel Read

--- AutomationCourse/src/main/java/pages/LoginPage.java
+++ AutomationCourse/src/main/java/pages/LoginPage.java
@@ -14,21 +14,18 @@ public class LoginPage {
 		//initElements used for initializing elements
 		PageFactory.initElements(driver,this);
 	}
-	@FindBy(id="user-name")WebElement username;
 	
+	@FindBy(id="user-name")WebElement username;
 	public void enterUserNameOnUsernameField() {
 		username.sendKeys("standard_user");
 	}
 	
 	@FindBy(id="password")WebElement password;
-	
-	
 	public void enterPasswordOnPasswordField() {
 		password.sendKeys("secret_sauce");
 	}
 	
 	@FindBy(id="login-button")WebElement loginBtn;
-	
 	public void clickOnLoginButton() {
 		loginBtn.click();
 	}
